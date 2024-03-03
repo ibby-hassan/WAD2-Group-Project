40cdf386022c68f65209761b6f5eb5b6c4997b01
ZhenYao Heng <2840506h@student.gla.ac.uk>
2024-03-03 23:00:51 +0000

    fixed the categories and questions so they actually show now

--- insQuire/urls.py
+++ insQuire/urls.py
@@ -6,5 +6,6 @@ app_name = 'insQuire'
 urlpatterns = [
     path('', views.index, name='index'),
     path('categories/', views.categories, name='all_categories'),
-    path('categories/<slug:slugifiedName>/', views.category, name='category')
+    path('categories/<slug:slugifiedName>/', views.category, name='category'),
+    path('question/<int:questionID>/', views.question, name='question'),
 ]
--- insQuire/views.py
+++ insQuire/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from insQuire.models import Category, Question
+from django.db import models
 
 def index(request):
     context = {}
@@ -6,8 +8,39 @@ def index(request):
 
 def categories(request):
     context = {}
+
+    try:
+        categories = Category.objects.all()
+        context['categories'] = categories
+
+    except Category.DoesNotExist:
+        context['categories'] = None
+
     return render(request, 'insQuire/categories.html', context)
 
 def category(request, slugifiedName):
     context = {}
+
+    try:
+        category = Category.objects.get(slugifiedName=slugifiedName)
+        questions = Question.objects.filter(category=category)
+        context['category'] = category
+        context['questions'] = questions
+
+    except Category.DoesNotExist:
+        context['category'] = None
+        context['questions'] = None
+
     return render(request, 'insQuire/category.html', context)
+
+def question(request, questionID):
+    context = {}
+
+    try:
+        question = Question.objects.get(id=questionID)
+        context['questions'] = question
+
+    except Question.DoesNotExist:
+        context['questions'] = None
+
+    return render(request, 'insQuire/question.html', context)
