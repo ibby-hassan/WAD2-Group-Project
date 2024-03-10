9a45b52c0975f26077e2d097f6ab6302580a9773
“Qiuran <2795305b@student.gla.ac.uk>
2024-03-10 18:17:07 +0000

    register/login done

--- insQuire_Project/settings.py
+++ insQuire_Project/settings.py
@@ -73,6 +73,12 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
