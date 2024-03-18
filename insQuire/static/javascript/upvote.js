6e6dac43c261df594f7065dda96a4cc363e7de3e
ZhenYao Heng <2840506h@student.gla.ac.uk>
2024-03-18 17:27:08 +0000

    removed some unused code

--- insQuire/static/javascript/upvote.js
+++ insQuire/static/javascript/upvote.js
@@ -1,29 +1,3 @@
 function vote(vote){
-    
+
 }
-
-// document.getElementById('upvote-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     var userAuthenticated = {{ user_authenticated|yesno:"true,false" }};
-//     if (!userAuthenticated) {
-//         alert('Not logged in');
-//         window.location.href = "{% url 'insQuire:login' %}";
-//     } else {
-//         var formData = new FormData(this);
-//         fetch(this.action, {
-//             method: 'POST',
-//             body: formData,
-//             headers: {
-//                 'X-CSRFToken': '{{ csrf_token }}'
-//             },
-//             credentials: 'same-origin'
-//         }).then(function(response) {
-//             if (response.ok) {
-//                 // Handle successful response
-//             } else {
-//                 // Handle error
-//             }
-//         });
-//     }
-// });
