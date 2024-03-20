3af272a5fac525a04e378f62cb58d30841ffcea9
ZhenYao Heng <2840506h@student.gla.ac.uk>
2024-03-20 16:48:52 +0000

    very very basic testing

--- insQuire/tests.py
+++ insQuire/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from insQuire.models import UserProfile, Question, User, Category
 
-# Create your tests here.
+class UserProfileModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='12345')
+        self.userprofile = UserProfile.objects.create(user=self.user, website='https://test.com')
+
+    def test_user_profile_creation(self):
+        self.assertEqual(self.userprofile.website, 'https://test.com')
+
+class QuestionModelTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name='Test Category')
+        self.question = Question.objects.create(title='Test Question', content='This is a test question', category=category)
+
+    def test_question_creation(self):
+        self.assertEqual(self.question.title, 'Test Question')
+        self.assertEqual(self.question.content, 'This is a test question')
+        self.assertEqual(self.question.category.name, 'Test Category')
