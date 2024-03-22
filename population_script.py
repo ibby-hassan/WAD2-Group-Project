049b374c2a02753cd037bbe936a34ddacc3bb294
Ibrahim Hassan <2667185H@student.gla.ac.uk>
2024-03-22 03:13:31 +0000

    Got the updated population script running.

--- population_script.py
+++ population_script.py
@@ -20,34 +20,34 @@ def populate():
     answerCooking = [
         {'content': 'I usually use 3 eggs for my omelet. I find that it is the perfect amount for me.', 'votes': 0},
         {'content': 'I usually cook my steak on a grill. I find that it is the best way to cook a steak.', 'votes': 0},
-        {'content': 'I love using carrots in my cooking. They add a nice sweet taste to my dishes.', 'votes': 0},
+        {'content': 'I love using carrots in my cooking. They add a nice sweet taste to my dishes.', 'votes': 0}
     ]
     cookingQuestions = [
-        {'title': 'How many eggs in your omelet?', 'content': 'Hi guys. I was curious, how many eggs should I be adding to my omelet?', 'answers':answerCooking[0], 'votes': 0},
-        {'title': 'What is the best way to cook a steak?', 'content': 'I have been trying to cook a steak for a while now, but I can never get it right. What is the best way to cook a steak?', 'answers':answerCooking[1], 'votes': 0},
-        {'title': 'Favourite vegetables?', 'content': 'What are your favourite vegetables to cook with?', 'answers':answerCooking[2], 'votes': 0}
+        {'title': 'How many eggs in your omelet?', 'content': 'Hi guys. I was curious, how many eggs should I be adding to my omelet?', 'answers':[answerCooking[0]], 'votes': 0},
+        {'title': 'What is the best way to cook a steak?', 'content': 'I have been trying to cook a steak for a while now, but I can never get it right. What is the best way to cook a steak?', 'answers':[answerCooking[1]], 'votes': 0},
+        {'title': 'Favourite vegetables?', 'content': 'What are your favourite vegetables to cook with?', 'answers':[answerCooking[2]], 'votes': 0}
     ]
 
     answerSport = [
         {'content': 'I find that practicing my shooting and dribbling has helped me improve my basketball skills.', 'votes': 0},
         {'content': 'I find that running long distances has helped me improve my marathon times.', 'votes': 0},
-        {'content': 'I love playing basketball. It is my favourite sport to play.', 'votes': 0},
+        {'content': 'I love playing basketball. It is my favourite sport to play.', 'votes': 0}
     ]
     sportsQuestions = [
-        {'title': 'How do I get better at basketball?', 'content': 'I have been playing basketball for a while now, but I am not improving. What can I do to get better?', 'answers':answerSport[0], 'votes': 0},
-        {'title': 'What is the best way to train for a marathon?', 'content': 'I have been training for a marathon for a while now, but I am not improving. What can I do to get better?', 'answers':answerSport[1], 'votes': 0},
-        {'title': 'Favourite sports?', 'content': 'What are your favourite sports to play?', 'answers':answerSport[2], 'votes': 0}
+        {'title': 'How do I get better at basketball?', 'content': 'I have been playing basketball for a while now, but I am not improving. What can I do to get better?', 'answers':[answerSport[0]], 'votes': 0},
+        {'title': 'What is the best way to train for a marathon?', 'content': 'I have been training for a marathon for a while now, but I am not improving. What can I do to get better?', 'answers':[answerSport[1]], 'votes': 0},
+        {'title': 'Favourite sports?', 'content': 'What are your favourite sports to play?', 'answers':[answerSport[2]], 'votes': 0}
     ]
 
     answerTech = [
         {'content': 'I love my iPhone. It is the best phone I have ever owned.', 'votes': 0},
         {'content': 'I love my Macbook. It is the best laptop I have ever owned.', 'votes': 0},
-        {'content': 'I love my Google Tablet. It is the best tablet I have ever owned.', 'votes': 0},
+        {'content': 'I love my Google Tablet. It is the best tablet I have ever owned.', 'votes': 0}
     ]
     technologyQuestions = [
-        {'title': 'What is the best phone to buy?', 'content': 'I am looking to buy a new phone, but I am not sure which one to get. What is the best phone to buy?', 'answers':answerTech[0], 'votes': 0},
-        {'title': 'What is the best laptop to buy?', 'content': 'I am looking to buy a new laptop, but I am not sure which one to get. What is the best laptop to buy?', 'answers':answerTech[1], 'votes': 0},
-        {'title': 'What is the best tablet to buy?', 'content': 'I am looking to buy a new tablet, but I am not sure which one to get. What is the best tablet to buy?', 'answers':answerTech[2], 'votes': 0}
+        {'title': 'What is the best phone to buy?', 'content': 'I am looking to buy a new phone, but I am not sure which one to get. What is the best phone to buy?', 'answers':[answerTech[0]], 'votes': 0},
+        {'title': 'What is the best laptop to buy?', 'content': 'I am looking to buy a new laptop, but I am not sure which one to get. What is the best laptop to buy?', 'answers':[answerTech[1]], 'votes': 0},
+        {'title': 'What is the best tablet to buy?', 'content': 'I am looking to buy a new tablet, but I am not sure which one to get. What is the best tablet to buy?', 'answers':[answerTech[2]], 'votes': 0}
     ]
 
     categories = {
@@ -82,7 +82,7 @@ def addUser(username, password, email):
 def addUserProfile(user):
     u = UserProfile.objects.get_or_create(user=user)[0]
     u.save()
-    return uP
+    return u
 
 def addCategory(name, description):
     c = Category.objects.get_or_create(name=name, description=description)[0]
