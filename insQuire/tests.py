787aadda5e523e0f8ac8de9f22a3886ff2a87697
Ibrahim Hassan <2667185H@student.gla.ac.uk>
2024-03-22 05:02:38 +0000

    implemented rudimental unit tests

--- insQuire/tests.py
+++ insQuire/tests.py
@@ -18,3 +18,24 @@ class QuestionModelTest(TestCase):
         self.assertEqual(self.question.title, 'Test Question')
         self.assertEqual(self.question.content, 'This is a test question')
         self.assertEqual(self.question.category.name, 'Test Category')
+    class CategoryModelTest(TestCase):
+        def setUp(self):
+            self.category = Category.objects.create(name='Test Category')
+        def test_category_creation(self):
+            self.assertEqual(self.category.name, 'Test Category')
+
+    class UserProfileModelTest(TestCase):
+        def setUp(self):
+            self.user = User.objects.create(username='testuser', password='12345')
+            self.userprofile = UserProfile.objects.create(user=self.user)
+        def test_user_profile_creation(self):
+            self.assertEqual(self.user.username, 'testuser')
+
+    class QuestionModelTest(TestCase):
+        def setUp(self):
+            category = Category.objects.create(name='Test Category')
+            self.question = Question.objects.create(title='Test Question', content='This is a test question', category=category)
+        def test_question_creation(self):
+            self.assertEqual(self.question.title, 'Test Question')
+            self.assertEqual(self.question.content, 'This is a test question')
+            self.assertEqual(self.question.category.name, 'Test Category')
